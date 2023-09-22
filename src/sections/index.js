41fa54129a4ecb213df23540e1e84da681fa632f
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-09-22 14:03:38 -0700

    Nike App. Landing Page

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import Subcribe from './Subcribe'
+import SuperQuality from './SuperQuality'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+
+export{
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subcribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer
+}
